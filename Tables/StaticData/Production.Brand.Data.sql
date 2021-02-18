5a78658454f2b5aa19f51f0170a2530b840ec3a1
DmitryB <dmitryb@devart.com>
2021-02-18 14:55:05 +0200

    Relapse Track with Spark and add Winner data.

--- Tables/StaticData/Production.Brand.Data.sql
+++ Tables/StaticData/Production.Brand.Data.sql
@@ -8,7 +8,14 @@ INSERT Production.Brand(BrandId, BrandName) VALUES (5, 'Ritchey')
 INSERT Production.Brand(BrandId, BrandName) VALUES (6, 'Strider')
 INSERT Production.Brand(BrandId, BrandName) VALUES (7, 'Sun Bicycles')
 INSERT Production.Brand(BrandId, BrandName) VALUES (8, 'Surly')
-INSERT Production.Brand(BrandId, BrandName) VALUES (9, 'Trek')
+INSERT Production.Brand(BrandId, BrandName) VALUES (9, 'Spark')
+
+GO
+SET IDENTITY_INSERT Production.Brand OFF
+GO
+SET IDENTITY_INSERT Production.Brand ON
+GO
+INSERT Production.Brand(BrandId, BrandName) VALUES (10, 'Winner')
 GO
 SET IDENTITY_INSERT Production.Brand OFF
 GO
