1e5acad0c1de6fd00019a008c79b6881dd3f2dc4
DmitryB <dmitryb@devart.com>
2021-02-26 11:55:39 +0200

    drop

--- Tables/StaticData/Production.Brand.Data.sql
+++ Tables/StaticData/Production.Brand.Data.sql
@@ -1,14 +1,6 @@
 ﻿SET IDENTITY_INSERT Production.Brand ON
 GO
-INSERT Production.Brand(BrandId, BrandName) VALUES (1, 'Electra')
-INSERT Production.Brand(BrandId, BrandName) VALUES (2, 'Haro')
-INSERT Production.Brand(BrandId, BrandName) VALUES (3, 'Heller')
-INSERT Production.Brand(BrandId, BrandName) VALUES (4, 'Pure Cycles')
-INSERT Production.Brand(BrandId, BrandName) VALUES (5, 'Ritchey')
-INSERT Production.Brand(BrandId, BrandName) VALUES (6, 'Strider')
-INSERT Production.Brand(BrandId, BrandName) VALUES (7, 'Sun Bicycles')
-INSERT Production.Brand(BrandId, BrandName) VALUES (8, 'Surly')
-INSERT Production.Brand(BrandId, BrandName) VALUES (9, 'Trek')
+
 GO
 SET IDENTITY_INSERT Production.Brand OFF
 GO
