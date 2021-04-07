128b45bb37a0d35a4c5a338546d80c0ef3033068
JordanSanders-Devart <62429421+JordanSanders-Devart@users.noreply.github.com>
2021-04-07 14:00:59 +0300

    Delete Tables/StaticData directory

--- Tables/StaticData/Sales.Store.Data.sql
+++
@@ -1,8 +0,0 @@
-﻿SET IDENTITY_INSERT Sales.Store ON
-GO
-INSERT Sales.Store(StoreId, StoreName, Phone, Email, Street, City, State, Zip) VALUES (1, 'Santa Cruz Bikes', '(831) 476-4321', 'santacruz@bikes.shop', '3700 Portola Drive', 'Santa Cruz', 'CA', '95060')
-INSERT Sales.Store(StoreId, StoreName, Phone, Email, Street, City, State, Zip) VALUES (2, 'Baldwin Bikes', '(516) 379-8888', 'baldwin@bikes.shop', '4200 Chestnut Lane', 'Baldwin', 'NY', '11432')
-INSERT Sales.Store(StoreId, StoreName, Phone, Email, Street, City, State, Zip) VALUES (3, 'Rowlett Bikes', '(972) 530-5555', 'rowlett@bikes.shop', '8000 Fairway Avenue', 'Rowlett', 'TX', '75088')
-GO
-SET IDENTITY_INSERT Sales.Store OFF
-GO
