1cadf67677df25de7977f4ef9d7cbb05a3998ca4
JordanSanders-Devart <DevartDemo@gmail.com>
2022-05-13 12:50:45 +0300

    Add a new column

--- Tables/Production.Brand.sql
+++ Tables/Production.Brand.sql
@@ -1,7 +1,10 @@
 ﻿CREATE TABLE [Production].[Brand] (
   [BrandId] [int] IDENTITY,
   [BrandName] [varchar](255) NOT NULL,
+  [BrandURL] [varchar](max) NULL,
+  [BrandIco] [varchar](max) NULL,
   PRIMARY KEY CLUSTERED ([BrandId])
 )
 ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
 GO
