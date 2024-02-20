854293ed776865ef7ecc6fb7bef35539d1ce2ec1
JordanSanders-Devart <DevartDemo@gmail.com>
2024-02-20 16:18:10 +0200

    Add a new view to list all contries with the table changes

--- Tables/Sales.Customer.sql
+++ Tables/Sales.Customer.sql
@@ -9,7 +9,12 @@
   [State] [varchar](25) NULL,
   [Zip] [varchar](5) NULL,
   [Skype] [varchar](255) NULL,
+  [CountryID] [int] NULL,
   PRIMARY KEY CLUSTERED ([CustomerId])
 )
 ON [PRIMARY]
+GO
+
+ALTER TABLE [Sales].[Customer]
+  ADD CONSTRAINT [FK_Customer_Country] FOREIGN KEY ([CountryID]) REFERENCES [Sales].[Country] ([CountryID])
 GO
